bfd329eab06b1d43c1436155befccca675306af6
Daniel Santos <daniel.santos@mvctech.ca>
2018-05-02 16:10:28 -0400

    Addition of fields to make consuming flexible.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.scss' ]
 } )
 export class AppComponent {
+  /* TODO: URL should be root format. The addition of '/wp-json/wp/v2/' is invisible to the user */
+  url = 'https://mvc-ca-web-stag.azurewebsites.net/wp-json/wp/v2/';
+  perCat = 2;
 }
