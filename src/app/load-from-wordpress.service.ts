ace361616d24550de2828b9fc8e66ae4f8ed84d9
Daniel Santos <daniel.santos@mvctech.ca>
2018-05-02 14:00:27 -0400

    Conforms the reception of responses to WP original API

--- src/app/load-from-wordpress.service.ts
+++ src/app/load-from-wordpress.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { OriginalWpPost } from "./data-classes/original-wp-post";
+import { OriginalWpMedia } from "./data-classes/original-wp-media";
 
 @Injectable()
 export class LoadFromWordpressService {
@@ -12,6 +14,11 @@ export class LoadFromWordpressService {
     // console.log('a - GET POSTS CALLED');
     url += 'posts?per_page=100';
     // console.log('b (TRANSFORMED URL) - ', url);
-    return this.http.get<Array<Object>>(url);
+    return this.http.get<Array<OriginalWpPost>>(url);
+  }
+
+  getWordpressMediaList(url: string) {
+    url += 'media?per_page=100';
+    return this.http.get<Array<OriginalWpMedia>>(url);
   }
 }
