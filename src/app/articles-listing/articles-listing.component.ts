a7fd2312136303838ae6fa2ee91f77cb1c3cfb80
Daniel Santos <daniel.santos@mvctech.ca>
2018-05-03 14:12:51 -0400

    Cleaned up as new master

--- src/app/articles-listing/articles-listing.component.ts
+++ src/app/articles-listing/articles-listing.component.ts
@@ -9,7 +9,8 @@ import { LoadFromWordpressService } from "../load-from-wordpress.service";
   styleUrls: [ 'articles-listing.scss' ],
   providers: [ LoadFromWordpressService ]
 } )
-export class ArticlesListingComponent implements OnChanges, OnInit {
+
+export class ArticlesListingComponent implements OnInit {
 
   @Input() url: string;
   @Input() perCat: number;
@@ -17,65 +18,28 @@ export class ArticlesListingComponent implements OnChanges, OnInit {
   medias: Array<WordpressMedia> = [];
   outputArray: Array<WordpressArticle> = [];
 
-  constructor( private loadFromWordpressService: LoadFromWordpressService ) {
-    this.url === undefined ? this.url = 'https://mvc-ca-web-stag.azurewebsites.net' : null;
-  }
+  constructor( private loadFromWordpressService: LoadFromWordpressService ) { }
 
   ngOnInit() {
-    this.updateHere( this.url );
+    this.matchWpCallToArrays( this.url );
   }
 
-  ngOnChanges( changes: { [ propKey: string ]: SimpleChange } ) {
-
-    let log: string[] = [];
-    for ( let propName in changes ) {
-      let changedProp = changes[ propName ];
-      let to = JSON.stringify( changedProp.currentValue );
-      // propName === 'url' ? this.url = to : null;
-      if ( changedProp.isFirstChange() ) {
-        log.push( `Initial value of ${propName} set to ${to}` );
-      } else {
-        let from = JSON.stringify( changedProp.previousValue );
-        log.push( `${propName} changed from ${from} to ${to}` );
-      }
-    }
-    console.log( log );
-  }
-
-  preSelectArticles( input: Array<WordpressArticle> ) {
-    // console.log( '******** Starts filtering the INPUT array' );
+  preSelectArticles( input: Array<WordpressArticle>, perCat ) {
     let counter = [];
-
     input.forEach( ( value, index, nInput ) => {
 
-      /*First if checks the input array for valid info*/
       if ( value._categories !== undefined && value._categories !== null ) {
-
-        /*Now the data is ok, let's see if we already have this category registered somewhere*/
         if ( counter[ value._categories ] !== undefined && counter[ value._categories ] !== null ) {
-          // Check the counter (less than 2) and increase it
-          if ( counter[ value._categories ] < this.perCat ) {
+          if ( counter[ value._categories ] < perCat ) {
             counter[ value._categories ]++;
-            // At the same time the 'post/article' is sent to the new OUTPUT array
             this.outputArray.push( value );
-            // console.log( index + ': Increased counter for ' + value._categories + '____ final:' + counter[ value._categories ] );
-          } else {
-            // console.log( index + ': Already full for category ' + value._categories + '____ final:' + counter[ value._categories ] );
           }
         } else {
-          /*OK, we didn't have the data, so let's add it*/
           counter[ value._categories ] = 1;
           this.outputArray.push( value );
-          // console.log( index + ': First insertion for ' + value._categories + '____ final:' + counter[ value._categories ] );
         }
-      } else {
-        /*If the input array has no valid data, report*/
-        // console.log( index, value._id + ' has no CATEGORY defined. REJECT' );
       }
     } );
-
-    // console.log( '******** Finished generating the OUTPUT array' );
-    // console.log( this.outputArray );
   }
 
   getImageUrl( mediaId: number ) {
@@ -85,13 +49,10 @@ export class ArticlesListingComponent implements OnChanges, OnInit {
     return sourceUrl;
   }
 
-  updateHere( url: string ) {
-    // console.log( 'UPDATE HERE CALLED: ', x );
+  matchWpCallToArrays( url: string ) {
     if ( url === '' ) {
-      console.log( url );
-      console.log( 'Do not move anything.' );
+      console.log( url, 'Do not move anything.' );
     } else {
-      // this.url = x;
       url += '/wp-json/wp/v2/';
       console.log( url );
 
@@ -123,9 +84,9 @@ export class ArticlesListingComponent implements OnChanges, OnInit {
               _htmlcontent_rendered: element.content.rendered
             } );
           } );
-          // console.log('LINE 66');
-          // console.log(this.articles);
-          this.preSelectArticles( this.articles );
+
+          this.preSelectArticles( this.articles, this.perCat );
+
         } );
     }
   }
